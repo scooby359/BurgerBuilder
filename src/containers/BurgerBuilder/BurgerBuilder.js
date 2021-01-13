714318e43c51b190faf6e08c51a72589f319f9e9
Chris Walton <chrisw@apadmi.com>
2021-01-13 14:49:17 +0000

    Show the burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react'
-import Auxillary from '../../hoc/Auxillary'
+import React, { Component } from 'react';
+import Auxillary from '../../hoc/Auxillary';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-    
-    
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+        },
+    };
+
     render() {
         return (
             <Auxillary>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Auxillary>
         );
