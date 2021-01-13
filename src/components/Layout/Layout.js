9cb5e0851cf221017e28c4c9b826308a9713be59
Chris Walton <chrisw@apadmi.com>
2021-01-13 13:26:38 +0000

    Initial layout done

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Auxillary from '../../hoc/Auxillary';
+import classes from './Layout.module.css'
+
+const layout = (props) => (
+    <Auxillary>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>{props.children}</main>
+    </Auxillary>
+);
+
+export default layout;
