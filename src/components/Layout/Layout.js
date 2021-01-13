8b0566f1da8502facf42dbdf0a7769f5d404b549
Chris Walton <chrisw@apadmi.com>
2021-01-13 15:19:59 +0000

    Fix linting

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Auxillary from '../../hoc/Auxillary';
-import classes from './Layout.module.css'
+import classes from './Layout.module.css';
 
 const layout = (props) => (
-    <Auxillary>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>{props.children}</main>
-    </Auxillary>
+  <Auxillary>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Auxillary>
 );
 
 export default layout;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Auxillary from '../../hoc/Auxillary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-    state = {
-        ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0,
-        },
-    };
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    },
+  };
 
-    render() {
-        return (
-            <Auxillary>
-                <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
-            </Auxillary>
-        );
-    }
+  render() {
+    return (
+      <Auxillary>
+        <Burger ingredients={this.state.ingredients} />
+        <div>Build Controls</div>
+      </Auxillary>
+    );
+  }
 }
 
 export default BurgerBuilder;
